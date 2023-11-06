29563ad5c636273fa1908b5f0f7de7c4f9c316af
ZackarySantana <zackzackyack@gmail.com>
2023-11-06 10:12:14 -0500

    Add index redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import type { MiddlewareResponseHandler } from "astro";
 import { exampleChangelog } from "./changelog";
 
-export const onRequest: MiddlewareResponseHandler = ({ locals }, next) => {
+export const onRequest: MiddlewareResponseHandler = (
+    { locals, url, redirect },
+    next,
+) => {
     locals.changelogs = exampleChangelog;
+
+    if (url.pathname === "/") {
+        return redirect("/changelog/" + locals.changelogs[0].displayName, 302);
+    }
+
     return next();
 };
