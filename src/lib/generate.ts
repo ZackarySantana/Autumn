320e74a78bd872bb384776a1ec7f05412869d8f9
Zackary Santana <zackzackyack@gmail.com>
2023-11-10 23:57:43 -0500

    Update the model and prompt to use

--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -35,14 +35,14 @@ export function generateMessage(pr: PR, recentPRs: PR[]): Promise<string[]> {
                 {
                     role: "system",
                     content:
-                        "You will be provided a pull request title and a list of other recent pull requests, write a succinct changelog relating to the pull request. You will use the other prs as context. Do not include the ticket label in the changelog, which might appear . Use a branded and professional tone and provide as much context for the changelog. Do not start it with a bullet or anything else. It can be up to 3 lines. Only send the summary.",
+                        "You will be provided a pull request title and body, as well as other recent pull request title's and body's. You will construct a changelog for the current pull request using the other pull requests as context. If the change is small, you can send nothing. If the change is large, you can send multiple lines. Do not include any ticket labels in the changelog. Use a professional tone that focuses on how users might be impacted by the change. Begin the changelog with a number 1-4 to indicate the type of change. 1: Bug Fix, 2: New Feature, 3: Improvement, 4: Other. If you do not know the type of change, use 4. Then a second number 1-4 to indicate the impact of the change. 1: Low, 2: Medium, 3: High, 4: Critical. If you do not know the impact of the change, use 1. For example a changelog might be: 1.4: Fixed a bug where the app would crash when opening a file.",
                 },
                 {
                     role: "user",
                     content: `Pull Request Title: "${pr.title}". Pull Request Body: "${pr.body}". Recent PRs: "${recentPRsText}")}`,
                 },
             ],
-            model: "gpt-3.5-turbo-16k",
+            model: "gpt-4-1106-preview",
         })
         .then((res) => {
             return res.choices[0].message.content?.split("\n") ?? [];
